6dcf0819825a9cd98f759b99c8b3473bfba2b7aa
carla <diasscarla@hotmail.com>
2025-04-11 17:44:15 -0300

    Adicionado filtro de  filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -96,9 +96,35 @@ app.delete("/movies/:id", async (req, res) => {
       where: { id },
     });
   } catch (error) {
-    return res.status(500).send({ message: "Não foi possível remover o filme"})
+    return res
+      .status(500)
+      .send({ message: "Não foi possível remover o filme" });
   }
   res.status(200).send();
+});
+
+app.get("/movies/:genreName", async (req, res) => {
+  console.log(req.params.genreName);
+
+  try {
+    const moviesFilteredByGenreName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: req.params.genreName,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+    res.status(200).send(moviesFilteredByGenreName);
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao filtrar filmes por gênero" });
+  }
 });
 
 app.listen(port, () => {
