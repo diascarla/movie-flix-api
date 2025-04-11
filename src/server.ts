cfb13257c8990eb381e729bc70f4a9647c5bf505
carla <diasscarla@hotmail.com>
2025-04-11 14:28:09 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,46 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
     orderBy: {
       title: "asc",
-    }, 
+    },
     include: {
       genres: true,
-      languages: true
-    }
+      languages: true,
+    },
   });
   res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  const movieWithSameTitle = await prisma.movie.findFirst({
+    where: { title: { equals: title, mode: "insensitive"}},
+  });
+
+  if(movieWithSameTitle){
+    return res.status(409).send({message: "Já existe um filme cadastrado com esse título"})
+  }
+
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Falha ao cadastrar filme" });
+  }
+  res.status(201).send();
 });
 
 app.listen(port, () => {
